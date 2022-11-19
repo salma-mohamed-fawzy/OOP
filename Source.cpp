0587ccae73a63e924620d3e3495945ffce4b56a2
salma-mohamed-fawzy <111157713+salma-mohamed-fawzy@users.noreply.github.com>
2022-11-19 20:11:59 +0200

    sum of nums using pointers)

--- Source.cpp
+++ Source.cpp
@@ -1,21 +1,16 @@
 #include<iostream>
 using namespace std;
-void swap(int &num1, int &num2,int *ptrA,int *ptrB)
-{
-	 ptrA = &num2;
-	 ptrB = &num1;
-}
 int main()
 {
-	int a, b;
-	int* ptrA = &b, *ptrB =& a;
 
-	cout <<"please enter the first number\n";
-	cin >> a;
-	cout << "please enter the second number\n";
-	cin >> b;
-	swap(a, b);
-	cout << "the first number is "<< *ptrB <<" and the second number is "<< *ptrA <<endl;
+	int a, b,c;
+	cout << "please enter the 2 numbers\n";
+	cin >> a >> b;
+	int* ptrA = &a;
+	int* ptrB = &b;
+	int* ptrC = &c;
+	*ptrC = *ptrA + *ptrB;             //rakezi hena fel satr da w tare2to
+	cout << "the sum of the two numbers is " << *ptrC << endl;
 	system("pause");
 	return 0;
 }
